551613189ab3671d5fcf06a713d274606ae0c569
Satish Balay <balay@mcs.anl.gov>
2015-10-12 21:58:53 -0500

    use setCompilers.LDFLAGS - not [pushlanguage(c)] getLinkerFlags() as this give CLINKERFLAGS

--- config/BuildSystem/config/package.py
+++ config/BuildSystem/config/package.py
@@ -1020,7 +1020,8 @@ class GNUPackage(Package):
     args.append('CFLAGS="'+self.removeWarningFlags(self.getCompilerFlags())+'"')
     args.append('AR="'+self.setCompilers.AR+'"')
     args.append('ARFLAGS="'+self.setCompilers.AR_FLAGS+'"')
-    args.append('LDFLAGS="'+self.getLinkerFlags()+'"')
+    if self.setCompilers.LDFLAGS:
+      args.append('LDFLAGS="'+self.setCompilers.LDFLAGS+'"')
     self.popLanguage()
     if hasattr(self.compilers, 'CXX'):
       self.pushLanguage('Cxx')
