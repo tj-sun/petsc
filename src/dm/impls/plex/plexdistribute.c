712b6f3a69cba30494b4e84047e394c3cf4526e6
Lawrence Mitchell <lawrence.mitchell@imperial.ac.uk>
2018-07-02 14:43:35 +0100

    plex: Fix pointSF creation if migrationSF specifies leaves
    If the leaves of the migrationSF are not NULL, then we should send
    leaves[p], not p, when voting on ownership.

--- src/dm/impls/plex/plexdistribute.c
+++ src/dm/impls/plex/plexdistribute.c
@@ -1511,8 +1511,8 @@ PetscErrorCode DMPlexCreatePointSF(DM dm, PetscSF migrationSF, PetscBool ownersh
         ierr = PetscSynchronizedPrintf(PetscObjectComm((PetscObject) dm), "[%d] Point %D RemotePoint %D Shift %D MyRank %D\n", rank, leaves ? leaves[p] : p, roots[p].index, (PetscInt) PetscRealPart(shift[roots[p].index%numShifts]), (rank + (shift ? (PetscInt) PetscRealPart(shift[roots[p].index%numShifts]) : 0))%size);CHKERRQ(ierr);
       }
       /* Either put in a bid or we know we own it */
-      leafNodes[p].rank  = (rank + (shift ? (PetscInt) shift[roots[p].index%numShifts] : 0))%size;
-      leafNodes[p].index = p;
+      leafNodes[p].rank  = (rank + (shift ? (PetscInt) PetscRealPart(shift[roots[p].index%numShifts]) : 0))%size;
+      leafNodes[p].index = leaves ? leaves[p] : p;
     }
     for (p = 0; p < nroots; p++) {
       /* Root must not participate in the reduction, flag so that MAXLOC does not use */
